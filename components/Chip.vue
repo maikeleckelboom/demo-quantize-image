dfcc888d4b4672b10e12de4f556aaa67b6a05fb9
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-02-01 00:28:26 +0100

    chore: fine-tune chip transition

--- components/Chip.vue
+++ components/Chip.vue
@@ -17,9 +17,11 @@ const slots = defineSlots<{
 
 const root = ref<HTMLElement>()
 
-const transitionDuration = ref<number>(320)
-const transitionDelay = ref<number>(160)
-const transitionTotalDuration = computed<number>(() => unref(transitionDuration) + unref(transitionDelay))
+const transitionDuration = ref<string>('200ms')
+const transitionDelay = ref<string>('160ms')
+const transitionTotalDuration = computed<number>(
+  () => parseInt(unref(transitionDuration)) + parseInt(unref(transitionDelay))
+)
 </script>
 
 <template>
@@ -32,8 +34,8 @@ const transitionTotalDuration = computed<number>(() => unref(transitionDuration)
     :data-with-icon="!!selectedIcon"
     class="chip-root"
   >
-    <Transition :duration="transitionDuration" name="chip">
-      <span v-if="selectedIcon" class="box-control">
+    <Transition :duration="transitionTotalDuration" name="chip">
+      <span v-if="selectedIcon" key="icon">
         <Icon :name="selectedIcon" class="icon" />
       </span>
     </Transition>
@@ -73,9 +75,14 @@ const transitionTotalDuration = computed<number>(() => unref(transitionDuration)
   grid-template-columns: 0 1fr;
   grid-template-rows: 1fr;
   place-content: start;
-  @apply text-label-lg font-medium;
+  @apply text-label-lg;
+  text-shadow: 0 1px 0 rgb(var(--on-surface-rgb) / 0);
 
-  transition: grid-template-columns 200ms ease-out;
+  transition:
+    grid-template-columns var(--_transition-duration) ease,
+    outline-color calc(var(--_transition-duration) * 0.4) ease,
+    background-color var(--_transition-duration),
+    text-shadow var(--_transition-duration);
   position: relative;
 
   .icon {
@@ -87,12 +94,10 @@ const transitionTotalDuration = computed<number>(() => unref(transitionDuration)
     top: 50%;
     transform: translateY(-50%) scale(0);
     opacity: 0;
-    transition: ease all 200ms;
-    transition-delay: 50ms;
-  }
-
-  .box-control {
-    overflow: hidden;
+    transition-duration: calc(var(--_transition-duration) * 0.4);
+    transition-property: transform, opacity;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-delay: 0ms;
   }
 
   &.selected {
@@ -100,6 +105,7 @@ const transitionTotalDuration = computed<number>(() => unref(transitionDuration)
     --_text-color: rgb(var(--on-surface-rgb));
     --_outline-color: rgb(var(--surface-tint-rgb) / 0.26);
     --_icon-color: rgb(var(--primary-rgb));
+    text-shadow: 0 1px 0 rgb(var(--on-surface-rgb) / 0.15);
 
     grid-template-columns: calc(var(--_icon-size) + 1px) 1fr;
 
@@ -109,7 +115,7 @@ const transitionTotalDuration = computed<number>(() => unref(transitionDuration)
     .icon {
       transform: translateY(-50%) scale(1);
       opacity: 1;
-      transition-delay: 0ms;
+      transition-delay: var(--_transition-delay);
     }
   }
 }
