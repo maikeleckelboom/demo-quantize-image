23b5ed2ce5458630b8991946e55dc2e739fb2d32
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-02-11 00:58:35 +0100

    chore: image flow & such

--- pages/quantize/index.vue
+++ pages/quantize/index.vue
@@ -112,53 +112,36 @@ function onCustomFIleChange(event: Event) {
       />
       <FileDropZone v-else @drop="onDrop" />
     </div>
-
-    <template v-if="selectedFile">
-      <div class="my-4">
-        <fieldset>
-          <div class="flex flex-col justify-between">
-            <label class="mb-2 flex flex-nowrap items-center gap-x-2 text-label-lg" for="maxColors">
-              Max Colors
-              <span class="text-xs tabular-nums text-on-surface-variant">(1-128)</span>
-              <Tooltip class="justify-self-end">
-                <button class="flex items-center justify-center">
-                  <Icon class="ml-0.5 h-4 w-4 text-on-surface-variant" name="ic:baseline-info" />
-                </button>
-                <template #content>
-                  The maximum number of colors to generate from the image.
-                </template>
-              </Tooltip>
-            </label>
-            <!--            <p class="text-body-sm text-on-surface-variant">
-                          The maximum number of colors to generate from the image.
-                        </p>-->
-
-            <div class="grid grid-cols-[1fr,50px]">
-              <div class="px-2">
-                <Slider v-model.number="maxColors" contained="true" max="128" min="1" step="1" />
-              </div>
-              <div class="px-2">
-                <NumericTextField id="maxColors" v-model.number="maxColors" max="128" min="1" />
-              </div>
-            </div>
+    <div v-if="!selectedFile" class="flex justify-end">
+      <Button :disabled="state.isLoadingExample" intent="text" size="sm" @click="loadExampleImage">
+        {{ state.isLoadingExample ? 'Loading...' : 'Load example image' }}
+      </Button>
+    </div>
+    <div class="my-2 grid grid-cols-[1fr,0.25fr]">
+      <div class="flex flex-col justify-between">
+        <label class="mb-2 flex flex-nowrap items-center gap-x-2 text-label-lg" for="maxColors">
+          Max Colors
+          <span class="text-xs tabular-nums text-on-surface-variant">(1-128)</span>
+          <Tooltip class="justify-self-end">
+            <button class="flex items-center justify-center">
+              <Icon class="ml-0.5 h-4 w-4 text-on-surface-variant" name="ic:baseline-info" />
+            </button>
+            <template #content> The maximum number of colors to generate from the image.</template>
+          </Tooltip>
+        </label>
+        <div class="grid grid-cols-[1fr,52px]">
+          <div class="px-2">
+            <Slider v-model.number="maxColors" contained="true" max="128" min="1" step="1" />
           </div>
-        </fieldset>
+          <div class="px-2">
+            <NumericTextField id="maxColors" v-model.number="maxColors" max="128" min="1" />
+          </div>
+        </div>
       </div>
-    </template>
-
-    <div class="my-4 flex flex-col">
+    </div>
+    <div class="mt-6 flex flex-col">
       <Buttons class="justify-end">
         <Button v-if="selectedFile" intent="text" @click="reset">Reset</Button>
-        <div v-else class="flex justify-end">
-          <Button
-            :disabled="state.isLoadingExample"
-            intent="outlined"
-            size="sm"
-            @click="loadExampleImage"
-          >
-            {{ state.isLoadingExample ? 'Loading...' : 'Load example image' }}
-          </Button>
-        </div>
         <Button :disabled="!selectedFile || state.isLoadingNextPage" @click="onExtractColors">
           {{ state.isLoadingNextPage ? 'Loading...,' : 'Extract colors' }}
         </Button>
